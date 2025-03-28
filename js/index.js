b84256f85b2344f7b2f0474760400d7dfef3d8b7
BryOk-droid <brianokoth534@gmail.com>
2025-03-28 10:37:57 +0300

    Select DOM elements

---
+++ js/index.js
@@ -0,0 +1,9 @@
+const petList = document.getElementById("pet-list");
+const likedList = document.getElementById("liked-pets");
+const loadDogsBtn = document.getElementById("load-dogs");
+const addDogForm = document.getElementById("add-dog-form");
+
+const confirmModal = document.getElementById("confirm-modal");
+const confirmText = document.getElementById("confirm-text");
+const confirmYes = document.getElementById("confirm-yes");
+const confirmNo = document.getElementById("confirm-no");
